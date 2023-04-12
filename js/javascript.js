2d5a1ec057a4a6e37e4f82643b6e93166a5583a1
fhros <112174015+fhros@users.noreply.github.com>
2023-04-12 12:33:00 +0300

    kissa update

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,6 @@
 var month = prompt("Kuukausi tähä");
 var answer = month.toLowerCase();
+var kissa = document.getElementById("kissa")
 
 var woawa = document.getElementById("kuukausi");
 
@@ -9,21 +10,12 @@ if (answer === "joulukuu" || answer === "tammikuu" || answer === "helmikuu") {
     woawa.innerText = "kevät on kai siisti ehkä";
 } else if(answer === "kesäkuu" || answer === "heinäkuu" || answer === "elokuu") {
     woawa.innerText = "kesä on siistii";
-} else if(answer === "syyskuu") {
+} else if(answer === "syyskuu" || answer === "lokakuu" || answer ==="marraskuu") {
     woawa.innerText = "syksy on siisti";
+} else if(answer === "kissa") {
+      kissa.src="https://th.bing.com/th/id/R.d69ea557142c01941a7337f29a43ac23?rik=mW20PnMIekH50g&pid=ImgRaw&r=0"
 } else {
     woawa.innerText = "Syötetty kuukausi ei kelpaa.";
 }
 
 console.log(woawa.innerText);
-
-
-
-var kissa = document.getElementById("kissa")
-
-var button = document.getElementById("btn")
-button.onclick = func
-
-function func() {
-    kissa.src="https://th.bing.com/th/id/R.d69ea557142c01941a7337f29a43ac23?rik=mW20PnMIekH50g&pid=ImgRaw&r=0"
-}
